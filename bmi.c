cf879f2f3787c235bb1d0278599eaacb5435d18d
AgustinGomez12 <assadog22@gmail.com>
2026-05-31 20:29:53 -0300

    correcion final

--- bmi.c
+++ bmi.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	//LINK A GITHUB => https://github.com/AgustinGomez12/trabajo-inform-tica-1-nro-3/blob/main/bmi.c
+    //LINK A GITHUB => https://github.com/AgustinGomez12/trabajo-inform-tica-1-nro-3/blob/main/bmi.c
 	int peso;
 	float altura;
 	
@@ -9,15 +9,20 @@ int main(void){
 	
 	printf("ingrese se altura en metros:");
 	scanf("%f",&altura);
-
+	
 	float imc = peso / (altura * altura);
-    //Los espacios son parte estetica de la vista final de la tabla.
-	printf("Su indiece de masa corporal es de: %.2f \n     Indice | Condicion \n     ------------------ \n      <18.5 | Bajo peso \n18.5 a 24.9 | Normal \n25.0 a 29.9 | Sobrepeso \n       >=30 | Obesidad\n",imc);
+	//
+	printf("Su indiece de masa corporal es de: %.2f \n",imc);
+	printf("    indice | Condicion\n---------------------------------\n");
+	printf("      <18.5|bajo peso\n");
+	printf("18.5 a 24.9|Normal\n");
+	printf("25.0 a 29.9|Sobrepeso\n");
+	printf("       >=30|Obesidad\n");
 	
-		return 0;
-	}
+	return 0;
+}
 	
 	
-
-
+	
+	
 
